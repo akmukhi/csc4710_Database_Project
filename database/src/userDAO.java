1b7c16515f1c03edd45b4dd9b161632d9d1bfa7b
rmkatbey <84145000+rmkatbey@users.noreply.github.com>
2022-10-26 20:47:11 -0400

    Update userDAO.java

--- database/src/userDAO.java
+++ database/src/userDAO.java
@@ -275,68 +275,154 @@ public class userDAO
         String[] INITIAL = {"drop database if exists testdb; ",
 					        "create database testdb; ",
 					        "use testdb; ",
-					        "drop table if exists User; ",
-					        ("CREATE TABLE if not exists User( " +
-					            "email VARCHAR(50) NOT NULL, " + 
-					            "firstName VARCHAR(10) NOT NULL, " +
-					            "lastName VARCHAR(10) NOT NULL, " +
-					            "password VARCHAR(20) NOT NULL, " +
-					            "birthday DATE NOT NULL, " +
-					            "adress_street_num VARCHAR(4) , "+ 
-					            "adress_street VARCHAR(30) , "+ 
-					            "adress_city VARCHAR(20)," + 
-					            "adress_state VARCHAR(2),"+ 
-					            "adress_zip_code VARCHAR(5),"+ 
-					            "cash_bal DECIMAL(13,2) DEFAULT 1000,"+ 
-					            "PPS_bal DECIMAL(13,2) DEFAULT 0,"+
-					            "PRIMARY KEY (email) "+"); ")
+						"drop table if exists User; ",
+						"CREATE TABLE if not exists User(" +
+						    "email VARCHAR(50) NOT NULL, " +
+						    "user_name VARCHAR(50) NOT NULL," +
+						    "firstName VARCHAR(10) NOT NULL," +
+						    "lastName VARCHAR(10) NOT NULL," +
+						    "password VARCHAR(20) NOT NULL," +
+						    "birthday DATE NOT NULL," +
+						    "address_street_num VARCHAR(4), " +
+						    "address_street VARCHAR(30), " +
+						    "address_city VARCHAR(20), " +
+						    "address_state VARCHAR(2), " +
+						    "address_zip_code VARCHAR(5), " +
+						    "cash_bal DECIMAL(13,2) DEFAULT 0, " +
+						    "PPS_bal DECIMAL(13,2) DEFAULT 0, " +
+						    "userid int NOT NULL AUTO_INCREMENT, " +
+						    "PRIMARY KEY (userid) "+"); ")
         					};
-        String[] TUPLES = {("insert into User(email, firstName, lastName, password, birthday, adress_street_num, adress_street, adress_city, adress_state, adress_zip_code, cash_bal, PPS_bal)"+
-        			"values ('susie@gmail.com', 'Susie ', 'Guzman', 'susie1234', '2000-06-27', '1234', 'whatever street', 'detroit', 'MI', '48202','1000', '0'),"+
-			    		 	"('don@gmail.com', 'Don', 'Cummings','don123', '1969-03-20', '1000', 'hi street', 'mama', 'MO', '12345','1000', '0'),"+
-			    	 	 	"('margarita@gmail.com', 'Margarita', 'Lawson','margarita1234', '1980-02-02', '1234', 'ivan street', 'tata','CO','12561','1000', '0'),"+
-			    		 	"('jo@gmail.com', 'Jo', 'Brady','jo1234', '2002-02-02', '3214','marko street', 'brat', 'DU', '54321','1000', '0'),"+
-			    		 	"('wallace@gmail.com', 'Wallace', 'Moore','wallace1234', '1971-06-15', '4500', 'frey street', 'sestra', 'MI', '48202','1000', '0'),"+
-			    		 	"('amelia@gmail.com', 'Amelia', 'Phillips','amelia1234', '2000-03-14', '1245', 'm8s street', 'baka', 'IL', '48000','1000', '0'),"+
-			    			"('sophie@gmail.com', 'Sophie', 'Pierce','sophie1234', '1999-06-15', '2468', 'yolos street', 'ides', 'CM', '24680','1000', '0'),"+
-			    			"('angelo@gmail.com', 'Angelo', 'Francis','angelo1234', '2021-06-14', '4680', 'egypt street', 'lolas', 'DT', '13579','1000', '0'),"+
-			    			"('rudy@gmail.com', 'Rudy', 'Smith','rudy1234', '1706-06-05', '1234', 'sign street', 'samo ne tu','MH', '09876','1000', '0'),"+
-			    			"('jeannette@gmail.com', 'Jeannette ', 'Stone','jeannette1234', '2001-04-24', '0981', 'snoop street', 'kojik', 'HW', '87654','1000', '0'),"+
-			    			"('root', 'default', 'default','pass1234', '0000-00-00', '0000', 'Default', 'Default', '0', '00000','1000','1000000000');")
+        String[] TUPLES = {("insert into User(email, user_name, firstName, lastName, password, birthday, adress_street_num, adress_street, adress_city, adress_state, adress_zip_code, cash_bal, PPS_bal)"+
+        			"values ('susie@gmail.com', 'SusieG', 'Susie ', 'Guzman', 'susie1234', '2000-06-27', '1234', 'whatever street', 'detroit', 'MI', '48202','1000', '0'),"+
+			    		 	"('don@gmail.com', 'DonC', 'Don', 'Cummings','don123', '1969-03-20', '1000', 'hi street', 'mama', 'MO', '12345','1000', '0'),"+
+			    	 	 	"('margarita@gmail.com', 'MargaritaL','Margarita', 'Lawson','margarita1234', '1980-02-02', '1234', 'ivan street', 'tata','CO','12561','1000', '0'),"+
+			    		 	"('jo@gmail.com', 'JoB', 'Jo', 'Brady','jo1234', '2002-02-02', '3214','marko street', 'brat', 'DU', '54321','1000', '0'),"+
+			    		 	"('wallace@gmail.com', 'WallaceM', 'Wallace', 'Moore','wallace1234', '1971-06-15', '4500', 'frey street', 'sestra', 'MI', '48202','1000', '0'),"+
+			    		 	"('amelia@gmail.com','AmeliaP', 'Amelia', 'Phillips','amelia1234', '2000-03-14', '1245', 'm8s street', 'baka', 'IL', '48000','1000', '0'),"+
+			    			"('sophie@gmail.com', 'SophieP','Sophie', 'Pierce','sophie1234', '1999-06-15', '2468', 'yolos street', 'ides', 'CM', '24680','1000', '0'),"+
+			    			"('angelo@gmail.com', 'AngeloF','Angelo', 'Francis','angelo1234', '2021-06-14', '4680', 'egypt street', 'lolas', 'DT', '13579','1000', '0'),"+
+			    			"('rudy@gmail.com', 'RudyS','Rudy', 'Smith','rudy1234', '1706-06-05', '1234', 'sign street', 'samo ne tu','MH', '09876','1000', '0'),"+
+			    			"('jeannette@gmail.com', 'JeannetteS','Jeannette ', 'Stone','jeannette1234', '2001-04-24', '0981', 'snoop street', 'kojik', 'HW', '87654','1000', '0'),"+
+			    			"('root', 'defaultuser','default', 'default','pass1234', '0000-00-00', '0000', 'Default', 'Default', '0', '00000','1000','1000000000');")
 			    			};
-	String[] INITIAL2 = {"drop table if exists Addresses;" +
-		"CREATE TABLE if not exists Addresses( " +
-		    "address_street_num VARCHAR(4), " +
-		    "address_street VARCHAR(30), " +
-		    "address_city VARCHAR(20), " +
-		    "address_state VARCHAR(2), " +
-		    "address_zip_code VARCHAR(5), " +
-		    "FOREIGN KEY(username) REFERENCES User(email) " +
-		    "address_id VARCHAR(5) NOT NULL auto_increment, " +
-		    "PRIMARY KEY(address_id) "+"); ")}; 
+	String[] INITIAL2 = {"use testdb;",
+    "CREATE TABLE if not exists Addresses( ",
+    "address_street_num VARCHAR(4), " +
+    "address_street VARCHAR(30),  " +
+    "address_city VARCHAR(20),  " +
+    "address_state VARCHAR(2),  " +
+    "address_zip_code VARCHAR(5),  " +
+    "username VARCHAR(50) NOT NULL,  " +
+    "userid int NOT NULL,  " +
+    "CONSTRAINT fk_userid FOREIGN KEY (userid) REFERENCES User(userid),  " +
+    "address_id int NOT NULL AUTO_INCREMENT,  " +
+    "PRIMARY KEY(address_id) "+"); ")}; 
+	    
 	String[] TUPLES2 = {("Insert into Addresses(address_street_num, address_street, address_city, address_state, address_zip_code, username)" +
-			     "values('1234','whatever street','detroit','MI','48202','susie@gmail.com')," +
-			     "values('1000', 'hi street', 'mama', 'MO', '12345','don@gmail.com')," +
-			     "values('1234', 'ivan street', 'tata','CO','12561','margarita@gmail.com')," +
-			     "values('3214','marko street', 'brat', 'DU', '54321','jo@gmail.com')," +
-			     "values('4500', 'frey street', 'sestra', 'MI', '48202','wallace@gmail.com')," +
-			     "values('1245', 'm8s street', 'baka', 'IL', '48000','amelia@gmail.com')," +
-			     "values('2468', 'yolos street', 'ides', 'CM', '24680','sophie@gmail.com')," +
-			     "values('4680', 'egypt street', 'lolas', 'DT', '13579','angelo@gmail.com')," +
-			     "values( '1234', 'sign street', 'samo ne tu','MH', '09876','rudy@gmail.com')," +
-			     "values( '0981', 'snoop street', 'kojik', 'HW', '87654','jeannette@gmail.com')," +
-			      "values('0000', 'Default', 'Default', '0', '00000','root');") };
+			     "values('1234','whatever street','detroit','MI','48202','SusieG',1)," +
+			     "('1000', 'hi street', 'mama', 'MO', '12345','DonC',2)," +
+			     "('1234', 'ivan street', 'tata','CO','12561','MargaritaL',3)," +
+			     "('3214','marko street', 'brat', 'DU', '54321','JoB',4)," +
+			     "('4500', 'frey street', 'sestra', 'MI', '48202','WallaceM',5)," +
+			     "('1245', 'm8s street', 'baka', 'IL', '48000','AmeliaP',6)," +
+			     "('2468', 'yolos street', 'ides', 'CM', '24680','SophieP',7)," +
+			     "('4680', 'egypt street', 'lolas', 'DT', '13579','AngeloF',8)," +
+			     "( '1234', 'sign street', 'samo ne tu','MH', '09876','RudyS',9)," +
+			     "( '0981', 'snoop street', 'kojik', 'HW', '87654','JeannetteS',10)," +
+			      "('0000', 'Default', 'Default', '0', '00000','defaultuser', 11);") };
+	    
+	    String[] INITIAL3 = {"drop table if exists NFT_Ledger;",
+"CREATE TABLE if not exists NFT_Ledger(" +
+    "NFT_name VARCHAR(150) NOT NULL, " +
+    "price DECIMAL(13,2) DEFAULT 0, " +
+    "FILENAME varchar(100) NOT NULL, " +
+    "description VARCHAR(100), " +
+    "userid int NOT NULL, " +
+    "CONSTRAINT fk_NFTuserid FOREIGN KEY (userid) REFERENCES User(userid), " +
+    "NFTid int NOT NULL auto_increment, " +
+   " PRIMARY KEY (NFTid) " + "); ")};
+	    
+	    String[] TUPLES3 = {"Insert into NFT_Ledger(NFT_name, price, FILENAME, description, userid)" +
+"values('CAT', 100.00, 'CATNFT', 'Cat NFT', 1), " +
+"('DOG', 150.00, 'DOGNFT', 'DOG NFT', 2), " +
+"('DOLPHIN', 150.00, 'DOLPHINNFT', 'DOLPHIN NFT', 3), " +
+"('WOLF', 150.00, 'WOLFNFT', 'WOLF NFT', 4), " +
+"('BAT', 150.00, 'BATNFT', 'BAT NFT', 5), " +
+"('HAWK', 150.00, 'HAWKNFT', 'HAWK NFT', 6), " +
+"('EAGLE', 150.00, 'EAGLENFT', 'EAGLE NFT', 7), " +
+"('BIRD', 150.00, 'BIRDNFT', 'BIRD NFT', 8), " +
+"('HORSE', 150.00, 'HORSENFT', 'HORSE NFT', 9), " +
+"('WHALE', 150.00, 'WHALENFT', 'WHALE NFT', 10), " +
+"('DEFAULT', 150.00, 'DEFAULTNFT', 'DEFAULT NFT', 11); ")};
+	    
+	    String[] INITIAL4 = {"drop table if exists Transaction_History;",
+("CREATE TABLE if not exists Transaction_History( " +
+	"NFTid int NOT NULL," +
+    "CONSTRAINT fk_TH_NFTId FOREIGN KEY(NFTid) REFERENCES NFT_Ledger(NFTid)," +
+    "creator_id VARCHAR(80)," +
+    "customer_id VARCHAR(80)," +
+    "transaction_date DATETIME," +
+    "transactionId int NOT NULL auto_increment," +
+    "PRIMARY KEY (transactionId) "+"); ")};
+	    String[] TUPLES4 = {"Insert Into Transaction_History(NFTid, creator_id, customer_id, transaction_date)" +
+"values (1,'SusieG', 'JeanetteS', '2022-10-26')," +
+"(2,'JeanetteS', 'SusieG', '2022-10-24')," +
+"(3,'SusieG', 'JoB', '2022-09-08')," +
+"(4,'AmeliaP', 'JeanetteS', '2022-05-23')," +
+"(5,'AngeloF', 'AmeliaP', '2022-07-06')," +
+"(6,'SusieG', 'RudyS', '2022-03-16')," +
+"(7,'SusieG', 'AngeloF', '2022-03-14')," +
+"(8,'RudyS', 'JeanetteS', '2022-01-20')," +
+"(9,'WallaceM', 'DonC', '2022-09-21')," +
+"(10,'SusieG', 'MargaritaL', '2022-10-21')," +
+"(11,'DonC', 'MargaritaL', '2022-07-01');" )};
+	    
+	    String[] INITIAL5 = {"drop table if exists Transfer_History;",
+    "CREATE TABLE if not exists Transfer_History(, " + 
+    "NFTid int NOT NULL, " + 
+    "CONSTRAINT fk_TrH_NFTId FOREIGN KEY(NFTid) REFERENCES NFT_Ledger(NFTid), " + 
+    "current_owner VARCHAR(60), " + 
+    "previous_owner VARCHAR(60), " + 
+    "transfer_date DATETIME, " + 
+    "transferId int NOT NULL auto_increment, " + 
+    "PRIMARY KEY (transferId) "+"); ")};
+	    
+	    String[] TUPLES5 = {"(Insert Into Transfer_History(NFTid, current_owner, previous_owner, transfer_date)" +
+"values(1,'SusieG', 'JeanetteS', '2022-10-26')," +
+"(2,'AmeliaP', 'DonC', '2022-09-15')," +
+"(3,'MargaritaL', 'RudyS', '2022-09-23')," +
+"(4,'SusieG', 'AmeliaP', '2022-08-15')," +
+"(5,'RudyS', 'JeanetteS', '2022-07-08')," +
+"(6,'DonC', 'MargaritaL', '2022-04-03')," +
+"(7,'SusieG', 'JeanetteS', '2022-10-20')," +
+"(8,'MargaritaL', 'JeanetteS', '2022-01-13')," +
+"(9,'RudyS', 'DonC', '2022-01-04')," +
+"(10,'SusieG', 'JeanetteS', '2022-03-05')," +
+"(11,'DonC', 'JeanetteS', '2022-06-17');" )};
 
         
         //for loop to put these in database
-        for (int i = 0; i < INITIAL.length; i++)
+for (int i = 0; i < INITIAL.length; i++)
         	statement.execute(INITIAL[i]);
-        for (int i = 0; i < TUPLES.length; i++)	
+for (int i = 0; i < TUPLES.length; i++)	
         	statement.execute(TUPLES[i]);
-	    for (int i = 0; i < INITIAL2.length; i++)	
+for (int i = 0; i < INITIAL2.length; i++)	
         	statement.execute(INITAL2[i]);
-	     for (int i = 0; i < TUPLES2.length; i++)	
+for (int i = 0; i < TUPLES2.length; i++)	
         	statement.execute(TUPLES2[i]);
+for (int i = 0; i < INITIAL3.length; i++)	
+        	statement.execute(INITAL3[i]);
+for (int i = 0; i < TUPLES3.length; i++)	
+        	statement.execute(TUPLES3[i]);
+for (int i = 0; i < INITIAL4.length; i++)	
+        	statement.execute(INITAL4[i]);
+for (int i = 0; i < TUPLES4.length; i++)	
+        	statement.execute(TUPLES4[i]);
+for (int i = 0; i < INITIAL5.length; i++)	
+        	statement.execute(INITAL5[i]);
+for (int i = 0; i < TUPLES5.length; i++)	
+        	statement.execute(TUPLES5[i]);
         disconnect();
     }
     
